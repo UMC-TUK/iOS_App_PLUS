d210e84c8010b4955e7b5cb558c4694152f92f03
JJ <jeonghojin03@gmail.com>
2023-03-28 20:49:18 +0900

    1주차 실습

---
+++ 해루:정호진/1주차/실습/FirstWeek_Practice/FirstWeek_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstWeek_Practice
+//
+//  Created by 정호진 on 2023/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
