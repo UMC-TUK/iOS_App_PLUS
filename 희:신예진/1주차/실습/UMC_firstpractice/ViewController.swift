c0327cd1abac72b77f14cab46f6266768fc1830a
yeahzxnn <yejin0621@tukorea.ac.kr>
2023-03-29 15:29:25 +0900

    1st week
    첫주차 과제

---
+++ 희:신예진/1주차/실습/UMC_firstpractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMC_firstpractice
+//
+//  Created by 신예진 on 2023/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
