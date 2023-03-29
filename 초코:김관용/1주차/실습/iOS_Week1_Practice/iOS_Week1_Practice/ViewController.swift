71437bc3461f4eb3cfbbc33f6e36d125054c52b2
Sammuelwoojae <79682208+Sammuelwoojae@users.noreply.github.com>
2023-03-29 20:09:53 +0900

    1주차 실습

---
+++ 초코:김관용/1주차/실습/iOS_Week1_Practice/iOS_Week1_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Week1_Practice
+//
+//  Created by 홍길동 on 2023/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
