9aeb767bd05d5435800d47a5873f6387458f8a0b
Jeong Dong <eejxdong99@gmail.com>
2023-03-29 17:33:48 +0900

    1주차 실습

---
+++ 제이디:이정동/1주차/실습/week1/week1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week1
+//
+//  Created by 이정동 on 2023/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
