e8f2fc87743300dfe7100ce22d450575dccfa3a7
yeahzxnn <yejin0621@tukorea.ac.kr>
2023-04-01 06:23:03 +0900

    1주차 미션 과제
    배너 작업까지 완

---
+++ 희:신예진/1주차/미션/UMC_firstweekmission/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMC_firstweekmission
+//
+//  Created by 신예진 on 2023/03/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
