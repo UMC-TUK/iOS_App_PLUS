e0d367851ed1357bf1e115c3dd901019dc1feef4
JJ <jeonghojin03@gmail.com>
2023-04-04 18:32:22 +0900

    2주차 실습

---
+++ 해루:정호진/2주차/실습/SecondWeek_Practice/SecondWeek_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondWeek_Practice
+//
+//  Created by 정호진 on 2023/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
