4955d9cec05da14dfc73229cc27cf1c8affd8f12
yeahzxnn <yejin0621@tukorea.ac.kr>
2023-04-04 17:28:01 +0900

    2주차 실습
    2주차 레이아웃 실습 완료했습니다.

---
+++ 희:신예진/2주차/실습/UMC_secondpractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMC_secondpractice
+//
+//  Created by 신예진 on 2023/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
