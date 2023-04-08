757834e12360c31a4f878df329f2024479f21705
Jeong Dong <eejxdong99@gmail.com>
2023-04-09 00:08:27 +0900

    3주차 실습

--- 제이디:이정동/3주차/실습/week3_practice/week3_practice/FirstViewController.swift
+++ 제이디:이정동/3주차/실습/week3_practice/week3_practice/FirstViewController.swift
@@ -9,21 +9,45 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
-    var data: String = "hello"
-
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var hobbyTextField: UITextField!
+    @IBOutlet weak var count: UILabel!
+    
+    var image: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        print(data)
-//    }
+    override func viewWillAppear(_ animated: Bool) {
+        guard let cnt = count.text, let num = Int(cnt) else { return }
+        count.text = "\(num + 1)"
+    }
+    
 
     @IBAction func buttonTapped(_ sender: UIButton) {
         let vc = storyboard?.instantiateViewController(identifier: "SecondVC") as! SecondViewController
-        vc.vc = self
+        
+        vc.image = image
+        vc.name = nameTextField.text
+        vc.hobby = hobbyTextField.text
+        
         self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func rpsTapped(_ sender: UIButton) {
+        self.image = sender.currentImage
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segue" {
+            let vc = segue.destination as! SecondViewController
+            
+            vc.image = image
+            vc.name = nameTextField.text
+            vc.hobby = hobbyTextField.text
+        }
     }
     
 }
--- 제이디:이정동/3주차/실습/week3_practice/week3_practice/SecondViewController.swift
+++ 제이디:이정동/3주차/실습/week3_practice/week3_practice/SecondViewController.swift
@@ -9,19 +9,34 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
-    var vc: FirstViewController?
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var name: String?
+    var hobby: String?
+    var image: UIImage?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupLabel()
+        
     }
     
-
-    @IBAction func buttonTapped(_ sender: UIButton) {
-        vc?.data = "hi"
-        navigationController?.popViewController(animated: true)
+    func setupLabel() {
+        
+        if let image = image {
+            imageView.image = image
+        } else {
+            imageView.image = UIImage(systemName: "person")
+        }
+        guard let name = name, let hobby = hobby else { return }
+        
+        label.text = "이름은 \(name), 취미는 \(hobby)."
     }
+    
     
 
 }
