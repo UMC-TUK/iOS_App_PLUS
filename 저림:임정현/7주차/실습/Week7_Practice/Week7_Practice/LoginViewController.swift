40f33260c121e184e8aca26514efba6823e23323
LimJeonghyun <22000617@handong.edu>
2023-05-16 01:48:53 +0900

    7주차 실습

---
+++ 저림:임정현/7주차/실습/Week7_Practice/Week7_Practice/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  Week7_Practice
+//
+//  Created by 우주대스타 on 2023-05-15.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
