8d16c7681a1fc8d1b5cfe27e7998bd8c3c0af38a
yeahzxnn <yejin0621@tukorea.ac.kr>
2023-05-21 00:24:45 +0900

    7주차 미션1

---
+++ 희:신예진/7주차/미션/미션1/UMC_seventhweekmission/AlarmTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  AlarmTableViewCell.swift
+//  UMC_seventhweekmission
+//
+//  Created by 신예진 on 2023/05/19.
+//
+
+import Foundation
+import UIKit
+
+
+class AlarmTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var AlarmTableViewCell: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
