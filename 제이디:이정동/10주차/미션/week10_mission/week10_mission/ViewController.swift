4c471a30030774813481c8b4c6141de43a04d979
Jeong Dong <eejxdong99@gmail.com>
2023-06-22 01:50:25 +0900

    10주차 미션

---
+++ 제이디:이정동/10주차/미션/week10_mission/week10_mission/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  week10_mission
+//
+//  Created by 이정동 on 2023/06/22.
+//
+
+import UIKit
+import KakaoSDKUser
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoTalk() success.")
+                    
+                    //do something
+                    _ = oauthToken
+                    
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+                    self.navigationController?.pushViewController(vc, animated: true)
+                }
+            }
+        }
+    }
+    
+}
+
